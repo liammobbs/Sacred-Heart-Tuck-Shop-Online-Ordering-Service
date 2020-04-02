e419dd30bb4e31d58a4b6028eaeeaddd7b41cc9c
Liam <liammobbs@gmail.com>
2020-04-02 16:20:03 +1300

    First commit

---
+++ django-ecommerce-master/core/forms.py
@@ -0,0 +1,75 @@
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+
+PAYMENT_CHOICES = (
+    ('B', 'Credit/Debit'),
+    ('C', 'Cash')
+)
+
+BREAK_CHOICES =(
+    'T', 'Morning Tea'
+    'L', 'Lunch'
+)
+
+class CheckoutForm(forms.Form):
+    pickup_date = forms.DateField(
+        input_formats=['%d/%m/%Y'],
+        widget=forms.DateTimeInput(attrs={
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1'
+        })
+    )
+    break_choice = forms.MultipleChoiceField(choices=BREAK_CHOICES)
+
+# class CheckoutForm(forms.Form):
+#     shipping_address = forms.CharField(required=False)
+#     shipping_address2 = forms.CharField(required=False)
+#     shipping_country = CountryField(blank_label='(select country)').formfield(
+#         required=False,
+#         widget=CountrySelectWidget(attrs={
+#             'class': 'custom-select d-block w-100',
+#         }))
+#     shipping_zip = forms.CharField(required=False)
+#
+#     billing_address = forms.CharField(required=False)
+#     billing_address2 = forms.CharField(required=False)
+#     billing_country = CountryField(blank_label='(select country)').formfield(
+#         required=False,
+#         widget=CountrySelectWidget(attrs={
+#             'class': 'custom-select d-block w-100',
+#         }))
+#     billing_zip = forms.CharField(required=False)
+#
+#     same_billing_address = forms.BooleanField(required=False)
+#     set_default_shipping = forms.BooleanField(required=False)
+#     use_default_shipping = forms.BooleanField(required=False)
+#     set_default_billing = forms.BooleanField(required=False)
+#     use_default_billing = forms.BooleanField(required=False)
+#
+#     payment_option = forms.ChoiceField(
+#         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Promo code',
+        'aria-label': 'Recipient\'s username',
+        'aria-describedby': 'basic-addon2'
+    }))
+
+#
+# class RefundForm(forms.Form):
+#     ref_code = forms.CharField()
+#     message = forms.CharField(widget=forms.Textarea(attrs={
+#         'rows': 4
+#     }))
+#     email = forms.EmailField()
+
+#
+# class PaymentForm(forms.Form):
+#     stripeToken = forms.CharField(required=False)
+#     save = forms.BooleanField(required=False)
+#     use_default = forms.BooleanField(required=False)
